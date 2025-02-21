e46d99447fa726cbe4de2ae9706b74234091b668
Shardul <66526618+ShardulVanage@users.noreply.github.com>
2025-02-21 13:48:32 +0530

    Update Content.jsx

--- src/app/(pages)/venue/Content.jsx
+++ src/app/(pages)/venue/Content.jsx
@@ -110,7 +110,7 @@ export default function VenuePage() {
           >
   
             <iframe
-           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7862661.399813475!2d105.91025889999999!3d15.793925249999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31157a4d736a1e5f%3A0xb03bb0c9e2fe62be!2sVietnam!5e0!3m2!1sen!2sin!4v1738576158072!5m2!1sen!2sin"
+           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d195.2664305983908!2d106.6661allowfullscreen="
               width="100%"
               height="100%"
               style={{ border: 0 }}
