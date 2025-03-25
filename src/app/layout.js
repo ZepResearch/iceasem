e287ef1b1dfbf002dd947d7d346e9d095446489e
ShardulVanage <vsharadul@gmail.com>
2025-03-25 11:42:15 +0530

    sorry galtise galate jagaha daldiya

--- src/app/layout.js
+++ src/app/layout.js
@@ -78,23 +78,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased mt-12`}
         >
           {/* <!-- Google tag (gtag.js) --> */}
-          <Script 
-          async 
-          src="https://www.googletagmanager.com/gtag/js?id=G-65D55ZHV55"
-          strategy="afterInteractive"
-        />
-        <Script 
-          id="google-analytics"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-65D55ZHV55');
-            `
-          }}
-        />
+         
 
         <Navbar/>
         {children}
